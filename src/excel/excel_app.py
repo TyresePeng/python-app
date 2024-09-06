fd6484d40de334079357d54ffe1b8a72a21877f7
peng.guo <peng.guo@loreal.com>
2024-09-06 15:08:45 +0800

    init sqlalchemy

--- src/excel/excel_app.py
+++ src/excel/excel_app.py
@@ -1,6 +1,6 @@
 import os
 import openpyxl
-
+import xlsxwriter
 path = r"/Users/peng.guo/Documents/code/python-app/src/excel"
 os.chdir(path)  # 修改工作路径
 
@@ -44,3 +44,20 @@ for i in range(len(sheets)):
         else:
             print(''.join([str(sheet.cell(row=r,column=c).value).ljust(20) for c in range(1,sheet.max_column+1)] ))
 
+# 创建一个名为【demo.xlsx】工作簿；
+workbook2 = xlsxwriter.Workbook("demo.xlsx")
+
+# 创建一个名为【2018年销售量】工作表；
+worksheet = workbook2.add_worksheet("2018年销售量")
+
+# 使用write_row方法，为【2018年销售量】工作表，添加一个表头；
+headings = ['产品', '销量', "单价"]
+worksheet.write_row('A1', headings)
+# 使用write方法，在【2018年销售量】工作表中插入一条数据；
+# write语法格式：worksheet.write(行,列,数据)
+
+data = ["苹果", 500, 8.9]
+for i in range(len(headings)):
+    worksheet.write(1, i, data[i])
+
+workbook2.close()
